0ba58bc4f96ac455b66718952070825350ff35d6
Schiz01d <chris@kuchu.net>
2012-07-10 00:42:25 +0300

    Changed to Vbo, made particles move around, added perlin noise and color depending on speed and direction.

--- ofCode/src/testApp.cpp
+++ ofCode/src/testApp.cpp
@@ -3,15 +3,17 @@
 //--------------------------------------------------------------
 void testApp::setup(){
 
-
 	// open an outgoing connection to HOST:PORT
 	sender.setup(HOST, PORT);
-    //ofBackgroundHex(0xff72a1);
+    ofBackgroundHex(0x000000);
     ofSetFrameRate(30);
-    ofSetWindowPosition(500, 500);    
+    ofSetWindowPosition(500, 500);
+	cam.setDistance(1000);
+	cam.setFarClip(10000);
+	center = ofVec3f(0,0,0);
     // this is our buffer to stroe the text data
-    ofBuffer buffer = ofBufferFromFile("/Users/ari/Projects/artistic/Import3dfromVRML/data/engefalos00.txt");
-    
+    //ofBuffer buffer = ofBufferFromFile("/Users/ari/Projects/artistic/Import3dfromVRML/data/engefalos00.txt");
+	ofBuffer buffer = ofBufferFromFile("engefalos00.txt");
     if(buffer.size()) { 
 		
         // we now keep grabbing the next line
@@ -20,20 +22,18 @@ void testApp::setup(){
             
             // move on to the next line
             string line = buffer.getNextLine();
-			
-            
-            // copy the line to draw later
-            // make sure its not a empty line
-            if(line.empty() == false) {
-                seussLines.push_back(line);
-            }
-            findPoint(line);
-            // print out the line
-            //cout << line << endl;
-            
+
+			// extract the vector and push it on the back of the list
+			ofVec3f tmpLine = findPoint(line);
+            points.push_back(tmpLine);
+			speeds.push_back(ofVec3f(ofRandomf()*2,ofRandomf()*2,ofRandomf()*2));
+			colors.push_back(ofFloatColor(1,1,1));
+			accelerations.push_back(ofVec3f(0,0,0));
+			initialPoints.push_back(tmpLine);
         }
-        
-    }	
+    }
+
+	
 }
 
 ofVec3f testApp::findPoint(string str) {
@@ -41,39 +41,43 @@ ofVec3f testApp::findPoint(string str) {
 	string tempString;
 	int tempPosition = str.find(" ");
 	tempString = str.substr(0,str.size());
-	tempVec.x = ofToFloat(tempString.substr(0,tempPosition));	
+	tempVec.x = ofToFloat(tempString.substr(0,tempPosition))*300;	
 	tempPosition = tempString.find(" ");
-	tempVec.y = ofToFloat(tempString.substr(tempPosition+1,tempPosition));
+	tempVec.y = ofToFloat(tempString.substr(tempPosition+1,tempPosition))*300;
 	tempString = tempString.substr(tempPosition+1,tempString.size());
 	tempPosition = tempString.find(" ");
-	tempVec.z = ofToFloat(tempString.substr(tempPosition+1,tempPosition));
+	tempVec.z = ofToFloat(tempString.substr(tempPosition+1,tempPosition))*300;
 	tempString = tempString.substr(tempPosition+1,tempString.size());
 	//tempString = tempString.substr(tempPosition+1,tempString.size());
-	cout << tempVec << endl;
-	points[counter] = tempVec;
-	counter++;
-	
-	
-	
-	
+	//cout << tempVec << endl;
 	return tempVec;
+	glPointSize(15);
 }
 
 //--------------------------------------------------------------
 void testApp::update(){
-
+	for (int i = 0 ; i < points.size(); i++){
+		speeds[i] += accelerations[i];
+		points[i] += speeds[i];
+		colors[i] = ofFloatColor(speeds[i].x,speeds[i].y,speeds[i].z);
+		accelerations[i] = 0;
+	}
+	pullToCenter();
+	applyPerlin();
 }
 
 //--------------------------------------------------------------
 void testApp::draw(){
-	for (int i =0; i < 14000; i++){
-		ofBox(300*points[i].x, 300*points[i].y, 300*points[i].z, 2);
-	}
+	cam.begin();
+	vbo.setColorData(&colors[0],colors.size(),GL_DYNAMIC_DRAW);
+	vbo.setVertexData(&points[0], points.size(), GL_DYNAMIC_DRAW);
+	vbo.draw(GL_POINTS, 0, (int)points.size());
+	cam.end();
 }
 
 //--------------------------------------------------------------
 void testApp::keyPressed(int key){
-	if(key == 'a' || key == 'A'){
+	/*if(key == 'a' || key == 'A'){
 		ofxOscMessage m;
 		m.setAddress("/test");
 		m.addIntArg(1);
@@ -81,7 +85,7 @@ void testApp::keyPressed(int key){
 		m.addStringArg("hello");
 		m.addFloatArg(ofGetElapsedTimef());
 		sender.sendMessage(m);
-	}
+	}*/
 }
 
 //--------------------------------------------------------------
@@ -89,13 +93,43 @@ void testApp::keyReleased(int key){
 
 }
 
+void testApp::pullToCenter() {
+	for (int i = 0 ; i < points.size(); i++){
+		ofVec3f dirToCenter	= points[i] - center;
+		float distToCenter	= dirToCenter.length();
+		float maxDistance	= 800.0f;
+	
+		if( distToCenter > maxDistance ){
+			dirToCenter.normalize();
+			float pullStrength = 0.0001f;
+			speeds[i] -= dirToCenter * ( ( distToCenter - maxDistance ) * pullStrength );
+		}
+	}
+}
+
+void testApp::applyPerlin() {
+for (int i = 0 ; i < points.size(); i++)
+{
+	float nX = points[i].x * 0.005f;
+	float nY = points[i].y * 0.005f;
+	float nZ = points[i].z * 0.005f;
+	float nW = ofGetFrameNum() * 0.0025f;
+	float noise = ofNoise( nX, nY, nZ );
+	float angle = noise * 15.0f;
+	ofVec3f noiseVector( cos( angle ), sin( angle ), cos(angle) );
+	speeds[i] += noiseVector  * 0.01f;
+}
+
+
+}
+
 //--------------------------------------------------------------
 void testApp::mouseMoved(int x, int y){
-	ofxOscMessage m;
+	/*ofxOscMessage m;
 	m.setAddress("/mouse/position");
 	m.addIntArg(x);
 	m.addIntArg(y);
-	sender.sendMessage(m);
+	sender.sendMessage(m);*/
 }
 
 //--------------------------------------------------------------
@@ -105,18 +139,18 @@ void testApp::mouseDragged(int x, int y, int button){
 
 //--------------------------------------------------------------
 void testApp::mousePressed(int x, int y, int button){
-	ofxOscMessage m;
+	/*ofxOscMessage m;
 	m.setAddress("/mouse/button");
 	m.addStringArg("down");
-	sender.sendMessage(m);
+	sender.sendMessage(m);*/
 }
 
 //--------------------------------------------------------------
 void testApp::mouseReleased(int x, int y, int button){
-	ofxOscMessage m;
+	/*ofxOscMessage m;
 	m.setAddress("/mouse/button");
 	m.addStringArg("up");
-	sender.sendMessage(m);
+	sender.sendMessage(m);*/
 
 }
 
--- ofCode/src/testApp.h
+++ ofCode/src/testApp.h
@@ -24,17 +24,22 @@ class testApp : public ofBaseApp {
 		void windowResized(int w, int h);
 		void dragEvent(ofDragInfo dragInfo);
 		void gotMessage(ofMessage msg);
+		void pullToCenter();
+		void applyPerlin();
 		ofVec3f findPoint(string srt);
-	
-		ofVec3f points[40000];
-	int counter;
-	ofTrueTypeFont font;
+		ofVec3f center;
+		vector<ofFloatColor> colors;
+		vector<ofVec3f> points;
+		vector<ofVec3f> speeds;
+		vector<ofVec3f> accelerations;
+		vector<ofVec3f> initialPoints;
+		float gravity;
+		ofVbo vbo;
+		ofEasyCam cam;
+		//ofVec3f points[40000];
+		//int counter;
+		ofTrueTypeFont font;
 		ofxOscSender sender;
-	
-		float           nextLetterTime;
-		int             lineCount;
-		int             letterCount;
-		vector <string> seussLines;	
 	
 
 
